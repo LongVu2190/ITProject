e547d97fd3b5b35d31d0f55db223cc57799934d2
LongVu2190 <hoanglongvu912@gmail.com>
2023-10-01 12:19:46 +0700

    a

--- authentication/auth.js
+++ authentication/auth.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
 export default function checkToken(req, res, next) {
-    // bypass login. register
+    // bypass login, register
     console.log("request url: " + req.url);
     if (req.url == '/user/login' || req.url == '/user/register') {
         next();
