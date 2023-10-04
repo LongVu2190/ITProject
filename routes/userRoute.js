70f82caa09dc78a31030fc55dec7c08944a1c2fd
LongVu2190 <hoanglongvu912@gmail.com>
2023-10-04 19:27:55 +0700

    a

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,7 +2,7 @@ import express from 'express';
 import {userController} from '../controllers/index.js'
 const router = express.Router();
 
-router.get('/login', userController.login);
+router.post('/login', userController.login);
 router.post('/register', userController.register);
 router.patch('/recharge', userController.rechargeBalance);
 
