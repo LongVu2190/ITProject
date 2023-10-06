c909de014a6195461f3437129a872680970534ac
LongVu2190 <hoanglongvu912@gmail.com>
2023-10-06 19:34:28 +0700

    format datetime

--- data/showTime/sql/getNowShowTime.sql
+++ data/showTime/sql/getNowShowTime.sql
@@ -6,5 +6,5 @@ SELECT 	DISTINCT
 		CONVERT(VARCHAR, mv.runTime, 108) as runTime,
 		mv.thumbnail,
 		mv.region
-FROM [dbo].[ShowTime_List] st inner join [dbo].[Movie_List] mv on st.movieId = '32e869oTz815Z0k2gEsH'
+FROM [dbo].[ShowTime_List] st inner join [dbo].[Movie_List] mv on st.movieId = mv.movieId
 WHERE st.[showingDate] >= CAST( GETDATE() AS Date )
