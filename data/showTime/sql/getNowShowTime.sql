05c7b48a69708fe0836bec1c872a1a31332679e6
LongVu2190 <hoanglongvu912@gmail.com>
2023-10-15 14:26:23 +0700

    update getNowShowTime

--- data/showTime/sql/getNowShowTime.sql
+++ data/showTime/sql/getNowShowTime.sql
@@ -7,5 +7,5 @@ SELECT 	DISTINCT
 		mv.thumbnail,
 		mv.region
 FROM [dbo].[ShowTime_List] st inner join [dbo].[Movie_List] mv on st.movieId = mv.movieId
-WHERE st.[showingDate] >= CAST( GETDATE() AS Date )
+WHERE st.[showingDate] > CAST( GETDATE() AS Date )
 AND st.[startTime] >= CONVERT(TIME, GETDATE())
