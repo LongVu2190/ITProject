01fa4148f5ab75b106bb88e02e221d78ea9dd4ea
LongVu2190 <hoanglongvu912@gmail.com>
2023-10-15 19:54:04 +0700

    update getTimes

--- data/showTime/sql/getTimes.sql
+++ data/showTime/sql/getTimes.sql
@@ -4,6 +4,5 @@ SELECT  st.showTimeId,
         CONVERT(VARCHAR, st.startTime, 108) AS startTime,
         CONVERT(VARCHAR, st.endTime, 108) AS endTime
 FROM [dbo].[ShowTime_List] st inner join [dbo].[Movie_List] mv on st.movieId = mv.movieId
-WHERE (st.[showingDate] = CAST(GETDATE() AS Date) AND st.[startTime] > CONVERT(TIME, GETDATE()))
-   OR (st.[showingDate] > CAST(GETDATE() AS Date))
-   AND mv.movieId = @movieId;
+WHERE (st.[showingDate] = CAST(GETDATE() AS Date) AND st.[startTime] > CONVERT(TIME, GETDATE()) AND mv.movieId = @movieId)
+   OR (st.[showingDate] > CAST(GETDATE() AS Date) AND mv.movieId = @movieId)
