8da6d72b6ae638c7d06b0122bcc69787c420515b
unknown <chgdat10a3@gmail.com>
2023-11-03 12:40:41 +0700

    Logout for user with persistLogin

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,90 +1,112 @@
 import { userQueries } from "../data/index.js";
+import refreshTokenController from "./refreshTokenController.js";
 
 const login = async (req, res, next) => {
-    try {       
-        const data = req.body;
-        console.log(req.body)
-        const response = await userQueries.login(data, res);
+  try {
+    const data = req.body;
+    // console.log(req.body);
+    const response = await userQueries.login(data, res);
 
-        res.send(response);
-    } catch (error) {
-        res.status(400).send(error.message);
-    }
+    res.send(response);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
 };
 
 const logout = async (req, res, next) => {
-    try {
-        res.clearCookie("jwt");
-        res.clearCookie("accessToken");
-        res.status(400).send({
-            message: "Log out"
-        })
-    } catch (error) {
-        res.status(400).send(error.message);
+
+    const cookies = req.cookies;
+    console.log(req.cookies)
+    if (!cookies?.jwt) return res.sendStatus(204); //No content
+    const refreshToken = cookies.jwt;
+    // Is refreshToken in db?
+    const foundUser = await refreshTokenController.checkRefreshToken('refreshToken');
+    if (!foundUser) {
+      res.clearCookie("jwt", {
+        httpOnly: true,
+        sameSite: "None",
+        secure: true,
+      });
+      res.clearCookie("accountId", {
+        httpOnly: true,
+        sameSite: "None",
+        secure: true,
+      });
+      return res.sendStatus(204);
     }
+
+    // Delete refreshToken in db
+    foundUser.refreshToken = "";
+    const result = await foundUser.save();
+    console.log(result);
+
+    res.clearCookie("jwt", { httpOnly: true, sameSite: "None", secure: true });
+    res.clearCookie("accountId", { httpOnly: true, sameSite: "None", secure: true });
+    res.sendStatus(204);
+
 };
 
 const register = async (req, res, next) => {
-    try {
-        const data = req.body;
-        const response = await userQueries.register(data);
+  try {
+    const data = req.body;
+    const response = await userQueries.register(data);
 
-        res.send(response);
-    } catch (error) {
-        res.status(400).send(error.message);
-    }
+    res.send(response);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
 };
 
 const getUser = async (req, res, next) => {
-    try {
-        const accountId = req.params.accountId;
-        const response = await userQueries.getUser(accountId);
+  try {
+    const accountId = req.params.accountId;
+    const response = await userQueries.getUser(accountId);
 
-        res.send(response);
-    } catch (error) {
-        res.status(400).send(error.message);
-    }
+    res.send(response);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
 };
 
 const getAllUser = async (req, res, next) => {
-    try {
-        const response = await userQueries.getAllUser();
+  try {
+    const response = await userQueries.getAllUser();
 
-        res.send(response);
-    } catch (error) {
-        res.status(400).send(error.message);
-    }
+    res.send(response);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
 };
 
 const rechargeBalance = async (req, res, next) => {
-    try {
-        const recharge = req.body.recharge;
-        const accountId = req.params.accountId;
-        const response = await userQueries.rechargeBalance(recharge, accountId);
+  try {
+    const recharge = req.body.recharge;
+    const accountId = req.params.accountId;
+    const response = await userQueries.rechargeBalance(recharge, accountId);
 
-        res.send(response);
-    } catch (error) {
-        res.status(400).send(error.message);
-    }
+    res.send(response);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
 };
 
 const getTicketsOfUser = async (req, res, next) => {
-    try {
-        const accountId = req.params.accountId;
-        const response = await userQueries.getTicketsOfUser(accountId);
+  try {
+    const accountId = req.params.accountId;
+    const response = await userQueries.getTicketsOfUser(accountId);
 
-        res.send(response);
-    } catch (error) {
-        res.status(400).send(error.message);
-    }
+    res.send(response);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
 };
 
 export default {
-    login,
-    logout,
-    register,
-    getUser,
-    rechargeBalance,
-    getTicketsOfUser,
-    getAllUser,
+  login,
+  logout,
+  register,
+  getUser,
+  rechargeBalance,
+  getTicketsOfUser,
+  getAllUser,
 };
