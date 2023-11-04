c1b46e5794e67c9795375f091673b525ed0bfedd
LongVu2190 <hoanglongvu912@gmail.com>
2023-11-04 19:51:28 +0700

    format date

--- data/user/sql/getTicketsOfUser.sql
+++ data/user/sql/getTicketsOfUser.sql
@@ -1,11 +1,11 @@
-SELECT mv.title,
-				mv.genre,
-				mv.region,
-				mv.cost,
-				st.showingDate,
-				st.startTime,
-				st.endTime,
-				tk.seatNumber				
+SELECT 	mv.title,
+		mv.genre,
+		mv.region,
+		mv.cost,
+        CONVERT(VARCHAR, st.showingDate, 23) AS showingDate,
+        CONVERT(VARCHAR, st.startTime, 108) AS startTime,
+        CONVERT(VARCHAR, st.endTime, 108) AS endTime,
+		tk.seatNumber				
 FROM Ticket_List tk INNER JOIN ShowTime_List st ON tk.showTimeId = st.showTimeId
 			INNER JOIN Movie_List mv on st.movieId = mv.movieId
 WHERE accountId = @accountId
