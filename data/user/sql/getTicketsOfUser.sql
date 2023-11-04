30c87a76a0abfbb11fc82111406058b02ff6c8c0
LongVu2190 <hoanglongvu912@gmail.com>
2023-11-04 20:08:06 +0700

    add id and date to ticket

--- data/user/sql/getTicketsOfUser.sql
+++ data/user/sql/getTicketsOfUser.sql
@@ -1,10 +1,12 @@
-SELECT 	mv.title,
+SELECT 	tk.ticketId,
+		mv.title,
 		mv.genre,
 		mv.region,
 		mv.cost,
         CONVERT(VARCHAR, st.showingDate, 23) AS showingDate,
         CONVERT(VARCHAR, st.startTime, 108) AS startTime,
         CONVERT(VARCHAR, st.endTime, 108) AS endTime,
+        CONVERT(VARCHAR, tk.purchaseDate, 23) AS purchaseDate,
 		tk.seatNumber				
 FROM Ticket_List tk INNER JOIN ShowTime_List st ON tk.showTimeId = st.showTimeId
 			INNER JOIN Movie_List mv on st.movieId = mv.movieId
