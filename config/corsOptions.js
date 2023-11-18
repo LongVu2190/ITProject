02ef1969795f58cd2de91ee55471fef026a5b950
LongVu2190 <hoanglongvu912@gmail.com>
2023-11-18 16:45:03 +0700

    update cors

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -4,8 +4,10 @@ const corsOptions = {
   origin: (origin, callback) => {
     callback(null, true);
   },
+  methods: 'GET,PUT,PATCH,POST,DELETE',
   optionsSuccessStatus: 200,
-  allowedHeaders: ["Content-Type"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
 };
 
 export default corsOptions;
