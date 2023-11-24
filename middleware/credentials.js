a7d84ec38753d65565a8d74b8e70462ce20511be
iamt4jdd <chgdat10a3@gmail.com>
2023-11-24 18:41:18 +0700

    Config corsOptions

--- middleware/credentials.js
+++ middleware/credentials.js
@@ -3,13 +3,13 @@ import allowedOrigins from "../config/allowedOrigins.js";
 const credentials = (req, res, next) => {
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Credentials", true);
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
       "Access-Control-Allow-Methods",
       "GET,OPTIONS,PATCH,DELETE,POST,PUT"
     );
-    res.setHeader(
+    res.header(
       "Access-Control-Allow-Headers",
       "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
     );
