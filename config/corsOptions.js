b065a1039c68ca3c357814bd477659507bd4e7a3
iamt4jdd <chgdat10a3@gmail.com>
2023-11-24 18:42:48 +0700

    Config corsOptions

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -2,13 +2,12 @@ import allowedOrigins from "./allowedOrigins.js";
 
 const corsOptions = {
   origin: (origin, callback) => {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
+    callback(null, true);
   },
+  methods: 'GET,PUT,PATCH,POST,DELETE',
   optionsSuccessStatus: 200,
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
 };
 
 export default corsOptions;
